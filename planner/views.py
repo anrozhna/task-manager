316f003af3ec608579fcbd5733142d74cbf52974
Anna <annalepiska@gmail.com>
2024-05-07 14:13:59 +0300

    add tasks as context in index view

--- planner/views.py
+++ planner/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
+from planner.models import Task
+
 
 def index(request: HttpRequest) -> HttpResponse:
     if request.user.is_authenticated:
-        return render(request, "planner/index.html")
+        tasks = Task.objects.filter(assignees=request.user)
+        context = {
+            "tasks": tasks
+        }
+        return render(request, "planner/index.html", context=context)
     else:
         return redirect("login")
