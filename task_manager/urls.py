20330fe20bb8543b3312f42f582dfefeaf0894cc
Anna <annalepiska@gmail.com>
2024-05-10 20:24:51 +0300

    add static files config

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path("", include("planner.urls", namespace="planner")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
