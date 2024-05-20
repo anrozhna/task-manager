ac825a7f637bd6e59f49abe5b94467e5457defa8
Anna <annalepiska@gmail.com>
2024-05-20 13:23:56 +0300

    update IndexView with get_context_data

--- planner/views.py
+++ planner/views.py
@@ -9,14 +9,30 @@ from planner.forms import (
     WorkerUpdateForm,
     UserRegistrationForm
 )
-from planner.models import Task
+from planner.models import Task, Position
 
 
 class IndexView(LoginRequiredMixin, generic.ListView):
     model = Task
-    template_name = "planner/worker_list.html"
+    template_name = "planner/index.html"
     paginate_by = 5
     queryset = Task.objects.prefetch_related("assignees")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        num_tasks = Task.objects.count()
+        num_workers = get_user_model().objects.count()
+        num_positions = Position.objects.count()
+        num_visits = self.request.session.get("num_visits", 0)
+        self.request.session["num_visits"] = num_visits + 1
+
+        context["num_tasks"] = num_tasks
+        context["num_workers"] = num_workers
+        context["num_positions"] = num_positions
+        context["num_visits"] = num_visits + 1
+
+        return context
 
 
 class WorkerListView(LoginRequiredMixin, generic.ListView):
