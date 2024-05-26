840d764144d3d9da718cbf7356fad3c5ffb47ea2
Anna <annalepiska@gmail.com>
2024-05-26 15:58:03 +0300

    add DJANGO_SECRET_KEY as SECRET_KEY

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-payuadp_f5&6skn2fm2@av1dpo2l)ze!fjys07+b*guw+*o=)7"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-payuadp_f5&6skn2fm2@av1dpo2l)ze!fjys07+b*guw+*o=)7")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
