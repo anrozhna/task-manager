d5ec0dffe47f13a5a3b032b14a1f12ad3c67e686
Anna <annalepiska@gmail.com>
2026-08-23 16:30:25 +0300

    chore: move ALLOWED_HOSTS to environment variable

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "task-manager-mul5.onrender.com"]
+ALLOWED_HOSTS = os.environ.get(
+    "DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
